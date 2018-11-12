376c3495aa5a154ba2fe8cb02afe130fdb7543f5
Rafael <rafael.pierre@gmail.com>
2018-11-12 20:40:38 +0100

    changes

--- week_2/automark.py
+++ week_2/automark.py
@@ -90,7 +90,11 @@ def _passed_local_tests(function, arg_keys):
         args_ = {k: in_[k] for k in arg_keys}
         answer = function(**args_)
         if not np.allclose(answer, out_, rtol=1e-5, atol=1e-5):
-            return False
+        	print('answer: {}'.format(answer))
+        	print('output: {}'.format(out_))
+        	return False
+
+    print('passed')
     return True
 
 
