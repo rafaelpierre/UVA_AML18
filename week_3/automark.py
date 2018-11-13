f93de3584206ae7fb3e2ce346731c62c748e5da0
Rafael <rafael.pierre@gmail.com>
2018-11-13 13:55:08 +0100

    finished version

--- week_3/automark.py
+++ week_3/automark.py
@@ -89,9 +89,9 @@ def _passed_local_tests(function, arg_keys):
     for in_, out_ in zip(inputs, outputs):
         args_ = {k: in_[k] for k in arg_keys}
         answer = function(**args_)
+        print('answer: {}'.format(answer))
+        print('output: {}'.format(out_))
         if not np.allclose(answer, out_, rtol=1e-5, atol=1e-5):
-            print('answer: {}'.format(answer))
-            print('output: {}'.format(out_))
             return False
     return True
 
